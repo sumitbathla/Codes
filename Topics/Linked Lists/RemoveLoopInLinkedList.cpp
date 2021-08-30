403a0f75fe1762f8aa607dfb77e8a1962aa22631
sumitbathla <87201197+sumitbathla@users.noreply.github.com>
2021-08-30 22:23:37 +0530

    Create RemoveLoopInLinkedList.cpp

---
+++ Topics/Linked Lists/RemoveLoopInLinkedList.cpp
@@ -0,0 +1,34 @@
+void removeLoop(Node* head)
+{
+    if(head==NULL || head->next==NULL){
+        return;
+    }
+    
+    struct Node* slow = head;
+    struct Node* fast = head;
+    
+    while(fast->next!=NULL && slow && fast && fast->next->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if(slow==fast){
+            break;
+        }
+    }
+
+    if(slow==fast){
+        slow = head;
+        if(slow==fast){
+            while(fast->next!=slow){
+                fast = fast->next;
+            }
+        } else{
+            while(slow->next!=fast->next){
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+        fast->next = NULL;
+    }
+    return;
+}
