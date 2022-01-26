d3d36ccd5f4f7325d1480cdb953fba83195dfa5f
sumitbathla <87201197+sumitbathla@users.noreply.github.com>
2022-01-26 18:16:03 +0530

    Create CountAndSay.cpp

---
+++ Topics/String/CountAndSay.cpp
@@ -0,0 +1,17 @@
+string countAndSay(int n) {
+    string s="1";
+    while(--n){
+        string ns;
+        int i=0;
+        while(i<s.length()){
+            int count=1;
+            while(s[i]==s[i+1] && i<s.length()){
+            i++;
+            count++;
+            }   
+            ns += to_string(count) + s[i];
+            i++;
+        }
+        s = ns;
+    }
+    return s;
