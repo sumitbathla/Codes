b6e2b73693d8500a541d189a0528ad5c7cdf3ebe
sumitbathla <87201197+sumitbathla@users.noreply.github.com>
2022-01-27 05:45:22 +0530

    Create LevelOrderTraversal.cpp

---
+++ Topics/Binary Trees/LevelOrderTraversal.cpp
@@ -0,0 +1,26 @@
+vector<int> levelOrder(Node* node)
+{
+    vector<int> res;
+    
+    if(node==NULL){
+        return res;
+    }
+    
+    queue<Node*> q;
+    q.push(node);
+    
+    while(q.size() > 0){
+        Node* temp = q.front();
+        res.push_back(temp->data);
+        q.pop();
+        
+        if(temp->left!=NULL){
+            q.push(temp->left);    
+        }
+        
+        if(temp->right!=NULL){
+            q.push(temp->right);
+        }
+    }
+    
+    return res;
