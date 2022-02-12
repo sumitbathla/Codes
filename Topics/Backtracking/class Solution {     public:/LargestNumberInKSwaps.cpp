92eb7be712581742f9a0b07ba70c4d2a67a78b55
sumitbathla <87201197+sumitbathla@users.noreply.github.com>
2022-02-12 22:50:13 +0530

    Create LargestNumberInKSwaps.cpp

---
+++ Topics/Backtracking/class Solution {     public:/LargestNumberInKSwaps.cpp
@@ -0,0 +1,47 @@
+class Solution
+{
+    public:
+    //Function to find the largest number after k swaps.
+    
+    void findMaximumNum(string str, int k, string& max, int ctr)
+    {
+        if (k == 0){
+            return;
+        }
+            
+        int n = str.length();
+     
+        char maxm = str[ctr];
+        for (int j = ctr + 1; j < n; j++) {
+            if (maxm < str[j])
+                maxm = str[j];
+        }
+     
+     
+        if (maxm != str[ctr])
+            k--;
+     
+        for (int j = ctr; j < n; j++) {
+            
+            if (str[j] == maxm) {
+                
+                swap(str[ctr], str[j]);
+                
+                if (str.compare(max)>0){
+                    max = str;
+                }
+                
+                findMaximumNum(str, k, max, ctr + 1);
+                swap(str[ctr], str[j]);
+            }
+            
+        }
+    }
+    
+    string findMaximumNum(string str, int k)
+    {
+        string max = str;
+        findMaximumNum(str, k, max, 0);
+ 
+        return max;
+    }
