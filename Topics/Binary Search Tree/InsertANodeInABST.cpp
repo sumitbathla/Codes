5c625fd4cb75005336305c4788906fef988a0ffd
sumitbathla <87201197+sumitbathla@users.noreply.github.com>
2022-02-16 22:51:16 +0530

    Update InsertANodeInABST.cpp

--- Topics/Binary Search Tree/InsertANodeInABST.cpp
+++ Topics/Binary Search Tree/InsertANodeInABST.cpp
@@ -3,9 +3,7 @@ Node* insert(Node* root, int Key)
     if(root==NULL){
         return new Node(Key);
     }
-    // if(root->data==Key){
-    //     return 0;
-    // }
+    
     if(root->data>Key){
         root->left = insert(root->left, Key);
     } else if(root->data<Key){
