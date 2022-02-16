2ef76e415492b5f3b5c171a807a0d3215c3f1929
sumitbathla <87201197+sumitbathla@users.noreply.github.com>
2022-02-16 22:50:20 +0530

    Create InsertANodeInABST.cpp

---
+++ Topics/Binary Search Tree/InsertANodeInABST.cpp
@@ -0,0 +1,16 @@
+Node* insert(Node* root, int Key)
+
+    if(root==NULL){
+        return new Node(Key);
+    }
+    // if(root->data==Key){
+    //     return 0;
+    // }
+    if(root->data>Key){
+        root->left = insert(root->left, Key);
+    } else if(root->data<Key){
+        root->right= insert(root->right, Key);
+    } else{
+        
+    }
+    return root;
