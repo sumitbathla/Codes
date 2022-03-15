eb41cfc3c7942a66b2e21ec43d8dac234aa5afe1
sumitbathla <87201197+sumitbathla@users.noreply.github.com>
2022-03-15 07:57:45 +0530

    Create MinimumSwapsToSort.cpp

---
+++ Topics/Searching & Sorting/MinimumSwapsToSort.cpp
@@ -0,0 +1,17 @@
+int minSwaps(vector<int>&nums){
+    vector<pair<int, int>> v(nums.size());
+    for(int i=0; i<nums.size(); i++){
+        v[i] = {nums[i], i};
+    }
+    sort(v.begin(), v.end());
+    int c=0;
+    for(int i=0; i<v.size(); i++){
+        if(v[i].second==i){
+            continue;
+        } else{
+            c++;
+            swap(v[i], v[v[i].second]);
+            i--;
+        }
+    }
+    return c
